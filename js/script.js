d072c3027254749686037d0020207c7ec96cf671
Maria Stachie <mary.stachie@gmail.com>
2022-03-03 15:32:18 +0100

    prova1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 console.log('JS OK');
+
+const nome = parseInt(prompt('Come ti chiami?'));
+
+const cognome = parseInt(prompt('Qual\'è il tuo cognome?'));
+
+const colorePreferito = parseInt(prompt('Qual\'è il tuo colore preferito?'));
+
+console.log('Il tuo nome è' + nome)
+
+document.getElementById(informazioni).innerHTML = 'Il tuo nome è' + nome
+

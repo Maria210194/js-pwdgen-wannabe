ea02878cf4b944f7ad54b851c5edcd884f7f00b6
Maria Stachie <mary.stachie@gmail.com>
2022-03-03 15:57:46 +0100

    done

--- js/script.js
+++ js/script.js
@@ -1,13 +1,25 @@
 console.log('JS OK');
 
-const nome = parseInt(prompt('Come ti chiami?'));
+const nome = (prompt('Come ti chiami?'));
 
-const cognome = parseInt(prompt('Qual\'è il tuo cognome?'));
+const cognome = (prompt('Qual\'è il tuo cognome?'));
 
-const colorePreferito = parseInt(prompt('Qual\'è il tuo colore preferito?'));
+const colorePreferito = (prompt('Qual\'è il tuo colore preferito?'));
+/*
+console.log('Il tuo nome è' + nome);
+document.getElementById('name').innerHTML = 'Il tuo nome è' + nome;
+console.log(document.getElementById('name').innerHTML)
 
-console.log('Il tuo nome è' + nome)
+console.log('Il tuo cognome è' + cognome);
+document.getElementById('surname').innerHTML = 'Il tuo cognome è' + cognome;
+console.log(document.getElementById('surname').innerHTML)
 
-document.getElementById('informazioni').innerHTML = 'Il tuo nome è' + nome;
-console.log(document.getElementById('informazioni').innerHTML)
 
+console.log('Il tuo colore preferito è' + colorePreferito);
+document.getElementById('favoriteColor').innerHTML = 'Il tuo colore preferito è' + colorePreferito;
+console.log(document.getElementById('favoriteColor').innerHTML)
+
+*/
+
+console.log(nome + cognome + colorePreferito);
+document.getElementById('informazioni').innerHTML = nome + cognome + colorePreferito + '21';

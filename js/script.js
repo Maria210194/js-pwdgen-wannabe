06763733b4ed7c37bd6db1b75d6693074d4820c4
Maria Stachie <mary.stachie@gmail.com>
2022-03-03 16:08:57 +0100

    text password before

--- js/script.js
+++ js/script.js
@@ -5,21 +5,22 @@ const nome = (prompt('Come ti chiami?'));
 const cognome = (prompt('Qual\'è il tuo cognome?'));
 
 const colorePreferito = (prompt('Qual\'è il tuo colore preferito?'));
-/*
+
+/* Non rispetta la consegna
 console.log('Il tuo nome è' + nome);
-document.getElementById('name').innerHTML = 'Il tuo nome è' + nome;
+document.getElementById('name').innerHTML = 'Il tuo nome è ' + nome;
 console.log(document.getElementById('name').innerHTML)
 
 console.log('Il tuo cognome è' + cognome);
-document.getElementById('surname').innerHTML = 'Il tuo cognome è' + cognome;
+document.getElementById('surname').innerHTML = 'Il tuo cognome è ' + cognome;
 console.log(document.getElementById('surname').innerHTML)
 
 
 console.log('Il tuo colore preferito è' + colorePreferito);
-document.getElementById('favoriteColor').innerHTML = 'Il tuo colore preferito è' + colorePreferito;
+document.getElementById('favoriteColor').innerHTML = 'Il tuo colore preferito è ' + colorePreferito;
 console.log(document.getElementById('favoriteColor').innerHTML)
 
 */
 
 console.log(nome + cognome + colorePreferito);
-document.getElementById('informazioni').innerHTML = nome + cognome + colorePreferito + '21';
+document.getElementById('informazioni').innerHTML = 'Password: ' + nome + cognome + colorePreferito + '21';
